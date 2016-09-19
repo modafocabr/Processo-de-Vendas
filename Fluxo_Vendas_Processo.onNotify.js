acbc53a1a95c15ffbc1976e82208d8ddc1ce36e0
Lucas Felipe <lucas.felipe.simoes@gmail.com>
2016-09-19 10:34:38 -0300

    Add files via upload

---
+++ Fluxo_Vendas_Processo.onNotify.js
@@ -0,0 +1,6 @@
+function onNotify(subject, receivers, template, params){
+	if (template == "TPLPROCESS_NEW_STATE_TO_MANAGER" != null) {
+        receivers.add("matheus.rocha@totvs.com");
+    }
+	
+}
